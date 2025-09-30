963aab00a1cfe530ff1f7c9507154d93fbbdfa99
Ben Mina <benjaminm@missionreadyhq.com>
2025-09-30 13:41:14 +1300

    MSH5-22-backend-server: Created endpoints for API Calls

---
+++ server.js
@@ -0,0 +1,49 @@
+// Module Imports
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+// Enable Express
+const app = express();
+
+// Middleware
+app.use(
+  cors({
+    origin: "http://localhost:5713",
+  })
+);
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI);
+
+// Initial Endpoint
+app.get("/", async (req, res) => {
+  console.log("The backend is connected");
+  res.json({ message: "The backend is connected" });
+});
+
+// Endpoint for Z-Stations
+app.post("/find-station", async (req, res) => {
+  console.log("Find station connected");
+  res.json({ message: "Seach endpoint connected" });
+});
+
+// Endpoint for Gas Station Prices
+app.post("/find-gas-prices", async (req, res) => {
+  console.log("Find gas prices connected");
+  res.json({ message: "Prices endpoint connected" });
+});
+
+// Endpoint for Current Location
+app.post("/find-current-location", async (req, res) => {
+  console.log("Find current location connected");
+  res.json({ message: "Current Location endpoint connected" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
